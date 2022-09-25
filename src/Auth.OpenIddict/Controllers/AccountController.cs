12404dbac9ce7c0bcc9997d7862be2a97c504db7
undrcrxwn <undrcrxwn@gmail.com>
2022-09-25 14:54:19 +0300

    Add OpenIddict

---
+++ src/Auth.OpenIddict/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using Auth.OpenIddict.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.OpenIddict.Controllers;
+
+public class AccountController : ControllerBase
+{
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginDto model)
+    {
+        if (!ModelState.IsValid)
+            return ValidationProblem(ModelState);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, model.Username)
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        await HttpContext.SignInAsync(new ClaimsPrincipal(claimsIdentity));
+
+        return Redirect(model.ReturnUrl);
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        return Ok();
+    }
+}
--- src/Auth.OpenIddict/Program.cs
+++ src/Auth.OpenIddict/Program.cs
@@ -1,15 +1,41 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseInMemoryDatabase(nameof(DbContext));
+    options.UseOpenIddict();
+});
+
+builder.Services.AddOpenIddict()
+    .AddCore(options => options
+        .UseEntityFrameworkCore()
+        .UseDbContext<DbContext>()
+    )
+    .AddServer(options => options
+        .AllowClientCredentialsFlow()
+        .SetTokenEndpointUris("/connect/token")
+        .AddEphemeralEncryptionKey()
+        .AddEphemeralSigningKey()
+        .RegisterScopes("api")
+        .UseAspNetCore()
+        .EnableTokenEndpointPassthrough()
+    );
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+        options.LoginPath = "/account/login");
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,6 +44,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
